69fe529d91a74b6b6449dcbf6323d1f0de09ad71
Administrator <Administrator@D29TUJV8ALS3BZT>
2021-08-31 14:14:16 +0700

    Add project files.

---
+++ MVC1/Controllers/GiaiPhuongTrinhController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+{
+    public class GiaiPhuongTrinhController : Controller
+    {
+        GiaiPTB1 gpt = new GiaiPTB1();
+        // GET: BaiTapGPTB1
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double HeSoA, double HeSoB)
+        {
+            double x = gpt.GPTB1(HeSoA, HeSoB);
+            ViewBag.NghiemPhuongTrinh = x;
+            return View();
+        }
+        public ActionResult NghiemPt()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC1/Models/GiaiPhuongTrinhBac1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC1.Models
+{
+    public class GiaiPhuongTrinhBac1
+
+    {
+       
+            public double GPTB1(double HeSoA, double HeSoB)
+            {
+                double x;
+                x = -HeSoB / HeSoA;
+                return x;
+            }
+        
+    }
+}
